1ead6b8316cd528663feb1aadbe02b2d0220b963
Ravi Prakash <ravi@lynkpeople.com>
2023-04-19 08:38:01 +0800

    Add debug json

--- backend/server/index.js
+++ backend/server/index.js
@@ -2,13 +2,19 @@ const express = require("express")
 const mongoose = require("mongoose")
 const store = require("./route/storeRoute")
 const user = require("./route/userRoute")
-const mongoURL = 'mongodb://mongo:27017'
+const axios = require("axios")
+const mongoURL = 'mongodb://localhost:27017'
+const rootURL = "https://httpbin.org/"
 app = express()
 app.use(express.json())
 app.use("/store", store)
 app.use("/user", user)
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/", async (req, res) => {
+    const url = rootURL+'get'
+    const response = await axios.get(url)
+    const data = response.data
+    console.log(data)
+    return res.json(data)
 })
 
 const port = 8080
