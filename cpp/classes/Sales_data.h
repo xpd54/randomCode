050bed791096fa416164014d59eb524b074806eb
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-08-19 15:39:16 +0800

    introducing Friend which allow function declaration to access private member.
    This let functions to access private member of class but this is not a memeber method yet.

--- cpp/classes/Sales_data.h
+++ cpp/classes/Sales_data.h
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 class Sales_data {
+  friend Sales_data add(const Sales_data &, const Sales_data &);
+  friend std::ostream &print(std::ostream &, const Sales_data &);
+  friend std::istream &read(std::istream &, Sales_data &);
+
 public:
   Sales_data() = default;
   Sales_data(std::istream &);
@@ -10,12 +14,10 @@ public:
 
   std::string isbn() const { return bookNo; }
   Sales_data &combine(const Sales_data &);
+
+private:
   double avg_price() const;
   std::string bookNo;
   unsigned units_sold = 0;
   double revenue = 0.0;
 };
-
-Sales_data add(const Sales_data &, const Sales_data &);
-std::ostream &print(std::ostream &, const Sales_data &);
-std::istream &read(std::istream &, Sales_data &);
