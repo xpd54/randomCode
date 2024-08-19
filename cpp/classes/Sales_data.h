61ffda74dc8ab460d2045f7a65bbea53a5e06ecd
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-08-19 15:41:14 +0800

    Friends are not member of class it's not affected by access control of class

--- cpp/classes/Sales_data.h
+++ cpp/classes/Sales_data.h
@@ -21,3 +21,7 @@ private:
   unsigned units_sold = 0;
   double revenue = 0.0;
 };
+
+Sales_data add(const Sales_data &, const Sales_data &);
+std::ostream &print(std::ostream &, const Sales_data &);
+std::istream &read(std::istream &, Sales_data &);
