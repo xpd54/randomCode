addb5e1b1a0b189cf744abcf91dfce2d96b50c1c
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-08-28 12:30:40 +0800

    Non initialized value does have garbage value but half initialized members have default value

--- cpp/oop/valueChecking.cpp
+++ cpp/oop/valueChecking.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <string>
+class BaseTest {
+public:
+  int value;
+};
 class Test {
 private:
   int private_value;
+  BaseTest test;
   std::string private_string;
 
 public:
   Test(std::string s) : private_string(s){};
   int public_value;
   void access_private() {
+    // Here test.value would have default 0 value
     std::cout << "Default private " << private_value << " -" << private_string
-              << "-" << std::endl;
+              << "-"
+              << " " << test.value << std::endl;
   }
   void access_public() {
     std::cout << "Default public " << public_value << std::endl;
@@ -22,5 +29,9 @@ int main() {
   Test test("Two Name");
   test.access_private();
   test.access_public();
+
+  // Here value would have garbage value 
+  BaseTest base_test;
+  std::cout << base_test.value << std::endl;
   return 0;
 }
