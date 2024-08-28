55a707b88c9d673f8659bf361ffdd8f1e09c1486
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-08-28 12:25:48 +0800

    If there is a constructor which initialized some member of class Other builtIn memeber would get initialized with default value as well.

--- cpp/oop/valueChecking.cpp
+++ cpp/oop/valueChecking.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <string>
 class Test {
 private:
   int private_value;
+  std::string private_string;
 
 public:
+  Test(std::string s) : private_string(s){};
   int public_value;
   void access_private() {
-    std::cout << "Default private " << private_value << std::endl;
+    std::cout << "Default private " << private_value << " -" << private_string
+              << "-" << std::endl;
   }
   void access_public() {
     std::cout << "Default public " << public_value << std::endl;
@@ -14,7 +18,8 @@ public:
 };
 
 int main() {
-  Test test = Test();
+  std::string input = "One Name";
+  Test test("Two Name");
   test.access_private();
   test.access_public();
   return 0;
