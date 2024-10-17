2daf60cd71350c9faf26b5680a543adb256eb4af
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-10-17 09:59:40 +0800

    Default template works save as defulat template for the class. To get defualt we need to skip the template parameter or use empty <>

--- cpp/template/default_temp.cpp
+++ cpp/template/default_temp.cpp
@@ -9,11 +9,26 @@ int compare(const T &v1, const T &v2, F f = F()) {
   return 0;
 }
 
+template <typename T = int> class Numbers {
+public:
+  Numbers(T v = 0) : val(v) {}
+  T getValue() { return val; }
+
+private:
+  T val;
+};
+
 int main() {
   auto less = [](int v1, int v2) { return v1 > v2; };
 
   int value1 = 20;
   int value2 = 230;
   std::cout << compare(20, 230, less) << std::endl;
+
+  Numbers<std::string> value("Call");
+  std::cout << value.getValue() << std::endl;
+
+  Numbers value_i(19);
+  std::cout << value_i.getValue() << std::endl;
   return 0;
 }
