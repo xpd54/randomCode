36f1ee509ca5b8a2d490d9170d5fcea9201d4bdb
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-11-19 12:57:46 +0800

    Add lambda function to create thread

--- cpp/concurrency/Thread.cpp
+++ cpp/concurrency/Thread.cpp
@@ -8,10 +8,18 @@ void test(int x) {
 
 int main() {
   std::thread myThread(&test, 100);
+
+  auto lambda = [](int x) {
+    std::cout << "Hello from lambda" << '\n';
+    std::cout << "Argument from lambda:- " << x << '\n';
+  };
+
+  std::thread lambda_thread(lambda, 100);
   std::cout << "hello from my main thread" << '\n';
 
   // main thread might finish it's work before test and whole program will get
   // terminated in that case it's core dump
   myThread.join();
+  lambda_thread.join();
   return 0;
 }
