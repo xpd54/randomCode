3adbc23fe39d6c3a3224ba9a20dfda0a4daa5b1b
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-11-22 17:55:45 +0800

    Update main for input error

--- cpp/io_library/main.cpp
+++ cpp/io_library/main.cpp
@@ -7,5 +7,17 @@ int main() {
   } catch (...) {
     std::cout << "Was an error" << std::endl;
   }
+
+  int val;
+  // If first error get thrown in stream.
+  /*Means if we put char rather than int in first input. Cause this istream had
+   * error for input it will throw error for next istream as well. And because
+   * of that it will print stream error rather than taking another input for
+   * next value.*/
+  if (std::cin >> val) {
+    std::cout << "Second value:- " << val << std::endl;
+  } else {
+    std::cout << "Stream had an error" << '\n';
+  }
   return 0;
 }
