21bad4d3325b13df7be606edb4cee6bd2a6006fa
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-12-17 13:56:48 +0800

    intermediatewq

--- cpp/Benchmark/src/main.cpp
+++ cpp/Benchmark/src/main.cpp
@@ -23,4 +23,31 @@ static void i32_addition_semi_random(bm::State &state) {
 }
 
 BENCHMARK(i32_addition_semi_random)->Threads(4);
+
+constexpr size_t f32s_in_cache_line_k = 64 / sizeof(float);
+constexpr size_t f32s_in_cache_line_half_k = f32s_in_cache_line_k / 2;
+
+struct alignas(64) f32_array_t {
+  float raw[f32s_in_cache_line_k * 2];
+};
+
+static void f32_pairwise_accumulation(bm::State &state) {
+  f32_array_t a, b, c;
+  for (auto _ : state)
+    for (size_t i = f32s_in_cache_line_half_k;
+         i != f32s_in_cache_line_half_k * 3; ++i)
+      bm::DoNotOptimize(c.raw[i] = a.raw[i] + b.raw[i]);
+}
+
+BENCHMARK(f32_pairwise_accumulation);
+
+static void f32_pairwise_accumulation_aligned(bm::State &state) {
+  f32_array_t a, b, c;
+  for (auto _ : state)
+    for (size_t i = 0; i != f32s_in_cache_line_half_k; ++i)
+      bm::DoNotOptimize(c.raw[i] = a.raw[i] + b.raw[i]);
+}
+
+BENCHMARK(f32_pairwise_accumulation_aligned);
+
 BENCHMARK_MAIN();
