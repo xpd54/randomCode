d00bdf4a65b2eb9463b3f970bea7c9b17d2723e1
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-12-17 12:01:19 +0800

    update with thread

--- cpp/Benchmark/src/main.cpp
+++ cpp/Benchmark/src/main.cpp
@@ -1,5 +1,7 @@
 #include <benchmark/benchmark.h>
 
+#include <cstdint>
+
 namespace bm = benchmark;
 static void i32_addition(bm::State &state) {
   int32_t a, b, c;
@@ -8,4 +10,17 @@ static void i32_addition(bm::State &state) {
 
 BENCHMARK(i32_addition);
 
+static void i32_addition_random(bm::State &state) {
+  int32_t c = 0;
+  for (auto _ : state) c = std::rand() + std::rand();
+}
+BENCHMARK(i32_addition_random)->Threads(4);
+
+static void i32_addition_semi_random(bm::State &state) {
+  int32_t sum = 0;
+  int32_t left = std::rand(), right = std::rand();
+  for (auto _ : state) bm::DoNotOptimize(sum = (++left) + (++right));
+}
+
+BENCHMARK(i32_addition_semi_random)->Threads(4);
 BENCHMARK_MAIN();
