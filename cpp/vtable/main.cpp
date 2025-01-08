b480128064135c77dbcafca473ed3d9500f138de
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-01-08 19:23:04 +0800

    Random vtable access with lldb lib

---
+++ cpp/vtable/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Parent {
+ public:
+  virtual void Foo() {}
+  virtual void FooNotOverridden() {}
+};
+
+class Derived : public Parent {
+ public:
+  void Foo() override {}
+};
+
+int main() {
+  Parent p1, p2;
+  Derived d1, d2;
+
+  std::cout << "done" << std::endl;
+}
