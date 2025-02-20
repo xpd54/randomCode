eceabf0917ed8634e9cfafb9c3317f3418029a0f
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-02-20 20:23:40 +0800

    Add object and non object parameter

--- cpp/template/square.cpp
+++ cpp/template/square.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <iostream>
 using namespace std;
 
@@ -8,7 +9,23 @@ T square(T input) {
 
 // c++20 abbreviated function templates, Can use auto in function for c++20
 auto square2(auto input) { return input * input; }
+
+template <typename T, typename C>
+void foo(T input1, C input2) {
+  cout << input1 << '\n';
+  cout << input2 << '\n';
+}
+
+// Non object parameter <object type, non object>
+template <typename T, size_t N>
+void print_time(T input) {
+  for (size_t i = 0; i < N; ++i) cout << input << '\n';
+}
 int main() {
   cout << square(3.3) << '\n';
   cout << square2(5) << '\n';
+
+  foo(5, 3.14f);
+  print_time<string, 4>("hello");
+  print_time<string, 2>("world");
 }
