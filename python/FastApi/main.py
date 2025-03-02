28d9eac669d9d1343cd2362c01c215db1a3a3ba6
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-03-03 07:13:39 +0800

    Add git ignore for fast api

---
+++ python/FastApi/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {"data": {"name": "ravi"}}
+
+
+@app.get("/about")
+def about():
+    return {"data": "about page"}
