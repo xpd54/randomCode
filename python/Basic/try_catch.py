f672b081508755d0e33ea2338006f308d0cbfccc
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-03-02 14:11:42 +0800

    Add try catch from all level of error

--- python/Basic/try_catch.py
+++ python/Basic/try_catch.py
@@ -5,3 +5,17 @@ try:
 except Exception as error:
     print("value is zero ", value)
     print(error)
+
+
+def catch_the_error() -> None:
+    value = input()
+    try:
+        int_val = int(value)
+        print(int_val)
+    except ValueError:
+        print("Error: Invalid value ")
+    except Exception as error:
+        print("Generic error ", error)
+
+
+catch_the_error()
