482cdfafc149a5cb769068ce99df145cce9a64f8
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-03-03 07:31:13 +0800

    localhost:8000/doc for swagger api

--- python/FastApi/main.py
+++ python/FastApi/main.py
@@ -11,3 +11,18 @@ def index():
 @app.get("/about")
 def about():
     return {"data": "about page"}
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+
+@app.get("/blog/{id}")
+def show(id):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id: int):
+    return {"data": {"comments": {"1", "2", id}}}
