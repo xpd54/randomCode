6194ec928457321699ff1a84e6b1afc40fce5cf8
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-03-02 11:54:25 +0800

    Add list iteration

--- python/Basic/list.py
+++ python/Basic/list.py
@@ -1,4 +1,19 @@
+from typing import List
+
+
 my_list = ["hello", "how", "are", "you", "doing"]
 
 for string in my_list:
     print(string)
+
+
+def count_x(nums: List[int], x: int) -> int:
+    count = 0
+    for value in nums:
+        if value == x:
+            count += 1
+    return count
+
+
+nums: List[int] = [1, 2, 3, 5, 56, 56, 5, 7, 8]
+print(count_x(nums, 5))
