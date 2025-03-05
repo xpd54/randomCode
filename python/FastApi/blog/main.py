f9aa93272c0c39cb2b36760b08902d697030ed06
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-03-05 08:14:14 +0800

    Update with https raise

--- python/FastApi/blog/main.py
+++ python/FastApi/blog/main.py
@@ -44,9 +44,12 @@ def delete_blog(id, db: Session = Depends(get_db)):
 
 @app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update(
-        {"title": request.title, "body": request.body}
-    )
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found"
+        )
+    blog.update({"title": request.title, "body": request.body})
     db.commit()
     return "updated"
 
