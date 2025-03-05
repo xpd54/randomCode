e65dba8f3d12cb762f044123a34c87ed70578af4
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-03-05 08:16:29 +0800

    finish crude

--- python/FastApi/blog/main.py
+++ python/FastApi/blog/main.py
@@ -37,7 +37,13 @@ def show_id(id, response: Response, db: Session = Depends(get_db)):
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_blog(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog:
+        raise HTTPException(
+            status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found"
+        )
+    blog.delete(synchronize_session=False)
     db.commit()
     return "done"
 
