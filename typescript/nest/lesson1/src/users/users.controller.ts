8eb8015a76e54bbc887d1d84ae793be626fdc8d0
Ravi Prakash <raviprakash.xpd54@gmail.com>
2025-03-09 22:54:09 +0800

    start with decorator route

--- typescript/nest/lesson1/src/users/users.controller.ts
+++ typescript/nest/lesson1/src/users/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /*
+  GET /users
+  GET /user/:id
+  POST /user
+  PATCH /user/:id
+  DELETE /user/:id
+  */
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return {id}
+  }
+
+  @Post() // 
+  create(@Body() user: {}) {
+    return user
+  }
+}
